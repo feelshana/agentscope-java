c6727063046729ba0a1f5e24a2927b1bd37c37b1
LearningGp <learninggp4.74@gmail.com>
2026-02-12 18:15:19 +0800

    Prepare 1.0.10-SNAPSHOT
    Change-Id: I38e8a9ae16eaa6229087475dcebe4c2cad5ddb1c

--- agentscope-core/src/main/java/io/agentscope/core/tool/mcp/McpClientBuilder.java
+++ agentscope-core/src/main/java/io/agentscope/core/tool/mcp/McpClientBuilder.java
@@ -296,7 +296,7 @@ public class McpClientBuilder {
 
                     McpSchema.Implementation clientInfo =
                             new McpSchema.Implementation(
-                                    "agentscope-java", "AgentScope Java Framework", "1.0.9");
+                                    "agentscope-java", "AgentScope Java Framework", "1.0.10-SNAPSHOT");
 
                     McpSchema.ClientCapabilities clientCapabilities =
                             McpSchema.ClientCapabilities.builder().build();
@@ -327,7 +327,7 @@ public class McpClientBuilder {
 
         McpSchema.Implementation clientInfo =
                 new McpSchema.Implementation(
-                        "agentscope-java", "AgentScope Java Framework", "1.0.9");
+                        "agentscope-java", "AgentScope Java Framework", "1.0.10-SNAPSHOT");
 
         McpSchema.ClientCapabilities clientCapabilities =
                 McpSchema.ClientCapabilities.builder().build();
